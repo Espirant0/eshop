34c5d765e093eadad7bc3a50efb7b7ce35beadb6
Mikhail Korol <mishakorol101@gmail.com>
2024-02-02 10:13:13 +0000

    Revert "Merge branch 'korol_branch' into 'main'"
    This reverts merge request !3

--- src/View/NotFoundPage/404.php
+++
@@ -1,5 +0,0 @@
-<div class="page_404_content">
-	<p class="page_404_message">Ошибка 404</p>
-	<p class="page_404_message">Страница не найдена</p>
-	<a href="../MainPage/index.php" class="page_404_return">На главную</a>
-</div>

d4e7363f3e0e40a7ca200ee0b6050245c532ec90
Mikhail Korol <mishakorol101@gmail.com>
2024-02-03 20:11:43 +0200

    Добавлены шаблоны

---
+++ src/View/NotFoundPage/404.php
@@ -0,0 +1,5 @@
+<div class="page_404_content">
+	<p class="page_404_message">Ошибка 404</p>
+	<p class="page_404_message">Страница не найдена</p>
+	<a href="../MainPage/index.php" class="page_404_return">На главную</a>
+</div>

eaa876a77ea5bac49edf5406d745b8c4589a2524
Михаил Бакиновский <wolfsector207@gmail.com>
2024-02-07 02:41:18 +0200

    Убраны комментарии для работы мигратора. Осуществлён перенос файла в src

--- src/Migration/2024.03.02_21.01_add_data_to_database.sql
+++ src/Migration/2024.03.02_21.01_add_data_to_database.sql
@@ -1,23 +1,22 @@
--- Добавление производителей
 INSERT INTO manufacturer (name) VALUES
 	('Trek'),
 	('Specialized'),
 	('Giant'),
 	('Cannondale');
 
--- Добавление категории
+
 INSERT INTO category (name) VALUES
 	('Горный'),
 	('Шоссейный'),
 	('Гибридный'),
 	('Электрический');
 
--- Добавление ролей
+
 INSERT INTO role (name) VALUES
 	('Администратор'),
 	('Пользователь');
 
--- Добавление статусов заказов
+
 INSERT INTO status (name) VALUES
 	('Подтвержден'),
 	('Отправлен'),
@@ -25,13 +24,13 @@ INSERT INTO status (name) VALUES
 	('Доставлен');
 
 
--- Добавление пользователей
+
 INSERT INTO user (id, name, role_id, address) VALUES
 	('123456789', 'Василий Васильев', 2, 'Советский проспект 5'),
 	('987654321', 'Петр Попов', 2, 'Тельмана');
 
 
--- Добавление товаров (велосипедов)
+
 INSERT INTO item (title, color, create_year, material, price, description, status, manufacturer_id)
 VALUES
 	('Trek Fuel EX 8', 'Черный', 2023, 'Carbon', 2999, 'Full suspension mountain bike', true, 1),
@@ -41,22 +40,22 @@ VALUES
 
 
 
--- Добавление изображений велосипедов
+
 INSERT INTO image (item_id, is_main, height, width) VALUES
 	(1, true, 800, 600),
 	(2, true, 800, 600),
 	(3, true, 800, 600),
 	(4, true, 800, 600);
 
--- Добавление категорий для товаров
+
 INSERT INTO items_category (item_id, category_id) VALUES
-	(1, 1), -- Trek Fuel EX 8 в категории горный
-	(2, 2), -- Specialized Roubaix в категории шоссейный
-	(3, 3), -- Giant Escape 3 в категории гибридный
-	(4, 2); -- Cannondale Synapse Neo в категории шоссейный
+	(1, 1),
+	(2, 2),
+	(3, 3),
+	(4, 2);
 
 
--- Добавление заказов
+
 INSERT INTO orders (item_id, status_id, data_create, price, user_id, address) VALUES
 	(1, 1, '2024-01-01', 2999, '123456789', 'Железнодорожная'),
 	(2, 2, '2024-01-02', 2499, '987654321', 'Красная');

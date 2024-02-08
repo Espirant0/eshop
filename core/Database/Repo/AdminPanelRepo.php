a9dde4d8182955f59faadfde606f6b590098bc42
Михаил Бакиновский <wolfsector207@gmail.com>
2024-02-08 15:43:32 +0200

    Запрос откорректирован для новой БД

--- core/Database/Repo/AdminPanelRepo.php
+++ core/Database/Repo/AdminPanelRepo.php
@@ -4,20 +4,23 @@ namespace Core\Database\Repo;
 
 use App\Model\User;
 use App\Model\Bicycle;
+use App\Service\DBHandler;
 
 class AdminPanelRepo extends BaseRepo
 {
 	public static function getItemList(): array
 	{
-		$connection = BaseRepo::getDbConnection();
+		$DBOperator = new DBHandler();
 		$itemList = [];
-		$result = mysqli_query($connection, "
-		SELECT i.id, i.title, i.color, i.create_year, i.material, i.price, i.description, i.status, m.name as vendor 
+		$result = $DBOperator->query("
+		SELECT i.id, i.title, c.name as color, i.create_year, m2.name as material, i.price, i.description, i.status, m.name as vendor 
 		FROM item i
-		         INNER JOIN manufacturer m on m.id = i.manufacturer_id
+		        INNER JOIN manufacturer m on m.id = i.manufacturer_id
+				INNER JOIN color c on i.color_id = c.id
+				INNER JOIN material m2 on i.material_id = m2.id
 		");
 		if (!$result) {
-			throw new Exception(mysqli_error($connection));
+			throw new \Exception($DBOperator->connect_error);
 		}
 		while ($row = mysqli_fetch_assoc($result))
 		{

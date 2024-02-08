c705f5bcd179dbfc3f83d2bd020702ce3ecb73cd
Obynochniy Alexandr <alexobyn655@gmail.com>
2024-02-08 22:14:56 +0200

    Полностью реализован CategoryListRepo, в конфиг добавлен словарь

--- config/config.php
+++ config/config.php
@@ -6,4 +6,15 @@ return [
     'DB_USER' => '',
     'DB_PASSWORD' => '',
     'DB_NAME' => '',
+    'CATEGORY_BLACK_LIST' => ['image','items_category','migration','role','status'],
+    'DICTIONARY' => [
+        'category' => 'Категория',
+        'color' => 'Цвет',
+        'item' => 'Товар',
+        'manufacturer' => 'Производитель',
+        'material' => 'Материал',
+        'orders' => 'Заказы',
+        'target_audience' => 'Целевая аудитория',
+        'user' => 'Пользователь',
+    ],
 ];

0a387cd912dba495830383cb4ee3b8cd474021f3
Михаил Бакиновский <wolfsector207@gmail.com>
2024-02-11 19:21:14 +0200

    Фикс перехвата id

--- src/Controller/DetailController.php
+++ src/Controller/DetailController.php
@@ -5,15 +5,17 @@ namespace App\Controller;
 use App\Model\Category;
 use App\Model\CategoryList;
 use Core\Database\Repo\CategoryListRepo;
+use Core\Database\Repo\DetailRepo;
 
 class DetailController extends BaseController
 {
-    public function showDetailPage($detail_id): void
+    public function showDetailPage($itemId): void
     {
+
         $categoryListRepo = new CategoryListRepo();
 
 		$this->render('layout.php', [
-			'content' => $this->strRender('DetailPage/detail.php', ['detail_id' => 0]),
+			'content' => $this->strRender('DetailPage/detail.php', ['item' => DetailRepo::getBicycleListById($itemId[0])]),
             'categoryList' => $categoryListRepo::getCategoryList()
 		]);
     }

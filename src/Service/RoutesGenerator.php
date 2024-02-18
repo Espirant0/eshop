6dcd89fb435cbb1edc6bbbe6dbb8f2b576282fba
Obynochniy Alexandr <alexobyn655@gmail.com>
2024-02-18 19:08:05 +0200

    Сделал автоматическую генерацию роутов

---
+++ src/Service/RoutesGenerator.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Service;
+
+use App\Config\Config;
+use App\Service\DBHandler;
+
+class RoutesGenerator
+{
+    public static function generateRoutes(): array
+    {
+        $config = new Config();
+        $DBOperator = new DBHandler();
+
+        $tables = $DBOperator->query("SHOW TABLES FROM {$config->option('DB_NAME')};");
+
+        $routes = [];
+
+        foreach ($tables as $table)
+        {
+            $tableName = is_array($table) ? reset($table) : $table;
+            $route = "/$tableName/:{$tableName}Name";
+            $routes[] = $route;
+        }
+
+        return $routes;
+    }
+
+    public static function addGenerateRoutesToCurrent(): void
+    {
+        $generatedRoutes = self::generateRoutes();
+
+        $currentRoutes = file_get_contents(ROOT . '/routesForFiltration.php');
+
+        foreach ($generatedRoutes as $route)
+        {
+            if (!str_contains($currentRoutes, $route))
+            {
+                $currentRoutes .= "\nRouter::get('$route', [new App\Controller\IndexController(), 'showIndexPage']);\n";
+            }
+        }
+
+        file_put_contents(ROOT . '/routesForFiltration.php', $currentRoutes);
+    }
+}

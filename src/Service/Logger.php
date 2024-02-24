8d5514a480c34896aa8d3f1d122eea005f55997c
Михаил Бакиновский <wolfsector207@gmail.com>
2024-02-25 01:26:16 +0200

    Апгрейт лога для работы с БД-ошибками

--- src/Service/Logger.php
+++ src/Service/Logger.php
@@ -4,18 +4,24 @@ namespace App\Service;
 
 class Logger
 {
-	private static $messageLine=1;
+	private static int $messageLine = 1;
+	private static string $errorLogPath = ROOT.'/var/logs';
+	private static function createLogDirectory(?string $logDir = null): void
+	{
+		if ($logDir == null) $logDir = self::$errorLogPath;
+		else $logDir = self::$errorLogPath."/".$logDir;
+		if(!file_exists($logDir))
+		{
+			mkdir($logDir,0777,true);
+		}
+	}
 	public static function writeErrorToLog(\ErrorException $info, string $trace = '', string $type = 'Error'):void
 	{
-		$errorLogPath = ROOT.'/var/logs';
 		$errorLogFile = ROOT."/var/logs/".date("Y-m-d")."-latest.txt";
 		$errorLogFileSize = 1024*1024;
 		$logString=date('[H-i-s]-')."Type=[$type]:\nOccurrence:".$info->getMessage().";\nwith code:[".$info->getCode()."];\nat:\nFile:".$info->getFile()." [Line:".$info->getLine()."]\n";
-		if(!file_exists($errorLogPath))
-		{
-			mkdir($errorLogPath,0777,true);
-		}
-		$logFiles = scandir($errorLogPath);
+		self::createLogDirectory();
+		$logFiles = scandir(self::$errorLogPath);
 		$logFiles = array_diff($logFiles, array('.', '..'));
 		sort($logFiles);
 		if(count($logFiles)==0) file_put_contents($errorLogFile,$logString);
@@ -23,13 +29,13 @@ class Logger
 		{
 			if(in_array('latest.txt',explode('-',$logFile)))
 			{
-				if(filesize($errorLogPath.'/'.$logFile) < $errorLogFileSize && (int)explode('-',$errorLogFile)[2]<=(int)(date('d')))
+				if(filesize(self::$errorLogPath.'/'.$logFile) < $errorLogFileSize && (int)explode('-',$errorLogFile)[2]<=(int)(date('d')))
 				{
-					file_put_contents($errorLogPath.'/'.$logFile,$logString, FILE_APPEND);
+					file_put_contents(self::$errorLogPath.'/'.$logFile,$logString, FILE_APPEND);
 				}
 				else
 				{
-					rename($errorLogPath.'/'.$logFile, $errorLogPath.'/'.explode('-latest',$logFile)[0].'.txt');
+					rename(self::$errorLogPath.'/'.$logFile, self::$errorLogPath.'/'.explode('-latest',$logFile)[0].'.txt');
 					file_put_contents($errorLogFile,$logString,FILE_APPEND);
 				}
 				break;
@@ -38,13 +44,20 @@ class Logger
 	}
 	public static function writeToLog(string $message):void
 	{
-
+		self::createLogDirectory();
 		$errorLogFile = ROOT."/var/logs/MESSAGELOG.txt";
 		if(self::$messageLine==1)
 		{
 			file_put_contents($errorLogFile,"----------новый прогон---------\n", FILE_APPEND);
 		}
-		file_put_contents($errorLogFile,"[".self::$messageLine."]".$message."\n", FILE_APPEND);
+		file_put_contents($errorLogFile,date('[H-i-s]--')."[".self::$messageLine."]".$message."\n", FILE_APPEND);
 		self::$messageLine++;
+	}
+	public static function ORMLogging(string $message, string $ORMfunction = 'ORM-work'): void
+	{
+		$specificDir = 'ORMlogs';
+		self::createLogDirectory($specificDir);
+		$errorLogFile = ROOT."/var/logs/$specificDir/ORMlog.txt";
+		file_put_contents($errorLogFile,date('[H-i-s] ').$message.' at '.$ORMfunction."\n", FILE_APPEND);
 	}
 }

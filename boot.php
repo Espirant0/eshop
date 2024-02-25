ad33eaa6d0e6aa1373b1259999285395a1a4a64b
Михаил Бакиновский <wolfsector207@gmail.com>
2024-02-25 22:39:21 +0200

    more const

--- boot.php
+++ boot.php
@@ -10,6 +10,11 @@ const LEFT = 2;
 const RIGHT = 3;
 const FULL = 4;
 const CROSS = 5;
+const AVERAGE = 1;
+const COUNT = 2;
+const SUM = 3;
+const MIN =4;
+const MAX = 5;
 #end of ORM flag constants
 require_once ROOT . '/vendor/autoload.php';
 require_once ROOT . '/config/config.php';

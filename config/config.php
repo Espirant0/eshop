25cf5b8f13d7544bcb24b79f35bacdb8ad3de3d8
Михаил Бакиновский <wolfsector207@gmail.com>
2024-03-02 13:09:05 +0200

    Расширен конфиг для лога

--- config/config.php
+++ config/config.php
@@ -33,4 +33,7 @@ return [
 	'IMAGE_DETAIL_WIDTH' => 650,
 	'IMAGE_ALLOWED_TYPES' => ['image/jpg', 'image/png', 'image/bmp', 'image/jpeg', 'image/webp'],
 	'IMAGE_MAX_SIZE' => 20971520,
+	'ERROR_LOG_FILE_SIZE' =>1024*1024,
+	'ORM_LOG_FILE_SIZE' => 1024*1024,
+	'LOG_FILE_DAYS_CAP' => 2,
 ];

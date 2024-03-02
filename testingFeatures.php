109bd1dd97a5f71da2db287462837d32d44f3cd8
Михаил Бакиновский <wolfsector207@gmail.com>
2024-03-02 13:11:52 +0200

    убраны константы

--- testingFeatures.php
+++ testingFeatures.php
@@ -1,20 +1,6 @@
 <?php
 
 const ROOT = __DIR__;
-#ORM flag constants
-const ASCENDING = 1;
-const DESCENDING = 2;
-const INNER = 1;
-const LEFT = 2;
-const RIGHT = 3;
-const FULL = 4;
-const CROSS = 5;
-const AVERAGE = 1;
-const COUNT = 2;
-const SUM = 3;
-const MIN =4;
-const MAX = 5;
-#end of ORM flag constants
 require_once ROOT . '/vendor/autoload.php';
 require_once ROOT . '/config/config.php';
 require_once ROOT . '/routes.php';

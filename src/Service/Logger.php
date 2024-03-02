cc4907aa265ae94aa943828ee5b05825d6555dc9
Михаил Бакиновский <wolfsector207@gmail.com>
2024-03-02 13:09:58 +0200

    Подправлен лог для более удобной работы с ORM-ошибками

--- src/Service/Logger.php
+++ src/Service/Logger.php
@@ -2,9 +2,10 @@
 
 namespace App\Service;
 
+use App\Config\Config;
+
 class Logger
 {
-	private static int $messageLine = 1;
 	private static string $errorLogPath = ROOT . '/var/logs';
 
 	private static function createLogDirectory(?string $logDir = null): void
@@ -16,51 +17,44 @@ class Logger
 			mkdir($logDir, 0777, true);
 		}
 	}
-
-	public static function writeErrorToLog(\ErrorException $info, string $trace = '', string $type = 'Error'): void
+	private static function sizeRelatedRenaming(string $logDir):void
 	{
-		$errorLogFile = ROOT . "/var/logs/" . date("Y-m-d") . "-latest.txt";
-		$errorLogFileSize = 1024 * 1024;
-		$logString = date('[H-i-s]-') . "Type=[$type]:\nOccurrence:" . $info->getMessage() . ";\nwith code:[" . $info->getCode() . "];\nat:\nFile:" . $info->getFile() . " [Line:" . $info->getLine() . "]\n";
-		self::createLogDirectory();
-		$logFiles = scandir(self::$errorLogPath);
+		$logDayCap = Config::getInstance()->option('LOG_FILE_DAYS_CAP');
+		$errorLogFileSize = Config::getInstance()->option('ERROR_LOG_FILE_SIZE');
+		$logPath = self::$errorLogPath.'/'.$logDir;
+		$logFiles = scandir($logPath);
 		$logFiles = array_diff($logFiles, array('.', '..'));
 		sort($logFiles);
-		if (count($logFiles) == 0) file_put_contents($errorLogFile, $logString);
+		if (count($logFiles) == 0)
+		{
+			file_put_contents($logPath . '/' . date("Y-m-d") . "-latest.txt", "Error log file initialization\n");
+		}
 		foreach ($logFiles as $logFile)
 		{
 			if (in_array('latest.txt', explode('-', $logFile)))
 			{
-				if (filesize(self::$errorLogPath . '/' . $logFile) < $errorLogFileSize && (int)explode('-', $errorLogFile)[2] <= (int)(date('d')))
+				if (filesize($logPath . '/' . $logFile) > $errorLogFileSize && abs((int)(date('d')) - (int)explode('-', $logFile)[2]) >= $logDayCap)
 				{
-					file_put_contents(self::$errorLogPath . '/' . $logFile, $logString, FILE_APPEND);
-				} else
-				{
-					rename(self::$errorLogPath . '/' . $logFile, self::$errorLogPath . '/' . explode('-latest', $logFile)[0] . '.txt');
-					file_put_contents($errorLogFile, $logString, FILE_APPEND);
+					rename($logPath . '/' . $logFile, $logPath . '/' . $logFile . '/' . explode('-latest', $logFile)[0] . '.txt');
 				}
-				break;
 			}
 		}
 	}
 
-	public static function writeToLog(string $message): void
+	public static function writeErrorToLog(\ErrorException $info, string $trace = '', string $type = 'Error'): void
 	{
-		self::createLogDirectory();
-		$errorLogFile = ROOT . "/var/logs/MESSAGELOG.txt";
-		if (self::$messageLine == 1)
-		{
-			file_put_contents($errorLogFile, "----------новый прогон---------\n", FILE_APPEND);
-		}
-		file_put_contents($errorLogFile, date('[H-i-s]--') . "[" . self::$messageLine . "]" . $message . "\n", FILE_APPEND);
-		self::$messageLine++;
+		$errorLogFile = ROOT . "/var/logs/errorLogs/" . date("Y-m-d") . "-latest.txt";
+		$logString = date('[H-i-s]-') . "Type=[$type]:\nOccurrence:" . $info->getMessage() . ";\nwith code:[" . $info->getCode() . "];\nat:\nFile:" . $info->getFile() . " [Line:" . $info->getLine() . "]\n";
+		self::createLogDirectory('errorLogs');
+		self::sizeRelatedRenaming('errorLogs');
+		file_put_contents($errorLogFile, $logString, FILE_APPEND);
 	}
 
 	public static function ORMLogging(string $message, string $ORMfunction = 'ORM-work'): void
 	{
-		$specificDir = 'ORMlogs';
-		self::createLogDirectory($specificDir);
-		$errorLogFile = ROOT . "/var/logs/$specificDir/ORMlog.txt";
+		$errorLogFile = ROOT . "/var/logs/ORMlogs/ORM-" . date("Y-m-d") . "-latest.txt";
+		self::createLogDirectory('ORMlogs');
+		self::sizeRelatedRenaming('ORMlogs');
 		file_put_contents($errorLogFile, date('[H-i-s] ') . $message . ' at ' . $ORMfunction . "\n", FILE_APPEND);
 	}
 }

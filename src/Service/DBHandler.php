fa56f84a7bfb23ddfdda71a436c70b9b5455e8e6
Mikhail Korol <mishakorol101@gmail.com>
2024-03-30 12:03:28 +0200

    Убран мёртвый код #1

--- src/Service/DBHandler.php
+++ src/Service/DBHandler.php
@@ -3,7 +3,6 @@
 namespace App\Service;
 
 use App\Config\Config;
-use Exception;
 
 class DBHandler extends \mysqli
 {
@@ -40,43 +39,9 @@ class DBHandler extends \mysqli
 		return self::$instance;
 	}
 
-	public function reconnect(): void
-	{
-		$this->close();
-
-		if (!$this->real_connect($this->dbHost, $this->dbUser, $this->dbPassword, $this->dbName))
-		{
-			throw new Exception($this->connect_error);
-		}
-	}
 
 	public function getResult(string $sqlQuery): array
 	{
 		return $this->query($sqlQuery)->fetch_all(MYSQLI_ASSOC);
 	}
-
-	public static function getResultStatic(string $sqlQuery): array|\Exception
-	{
-		$config = Config::getInstance();
-
-		$connection = mysqli_init();
-		$connected = mysqli_real_connect(
-			$connection,
-			$config->option('DB_HOST'),
-			$config->option('DB_USER'),
-			$config->option('DB_PASSWORD'),
-			$config->option('DB_NAME')
-		);
-
-		if (!$connected)
-		{
-			return new Exception($connection->connect_error);
-		}
-
-		$connection->set_charset('utf8');
-
-		return $connection->query($connection->real_escape_string($sqlQuery))->fetch_all(MYSQLI_ASSOC);
-	}
-
-
 }
